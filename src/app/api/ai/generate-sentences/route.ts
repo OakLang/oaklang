5a0edf53524a3d159cfd3a8337d9775547825959
Rohid <rohidul209@gmail.com>
2024-05-29 23:20:39 +0600

    uncomment generate sentence code

--- src/app/api/ai/generate-sentences/route.ts
+++ src/app/api/ai/generate-sentences/route.ts
@@ -25,180 +25,180 @@ const buildPrompt = ({
 };
 
 export const POST = async (req: NextRequest) => {
-  return NextResponse.json({
-    knownVocabs: [],
-    practiceVocabs: [
-      'el',
-      'casa',
-      'ser',
-      'grande',
-      'el',
-      'gato',
-      'comer',
-      'ella',
-      'ser',
-      'mío',
-      'amiga',
-      'tener',
-      'un',
-      'libro',
-      'el',
-      'perro',
-      'ser',
-      'bonito',
-    ],
-    sentences: [
-      {
-        lexicons: [
-          {
-            ipa: 'la',
-            lemma: 'el',
-            lexicon: 'La',
-            translation: 'The',
-          },
-          {
-            ipa: 'ˈkasa',
-            lemma: 'casa',
-            lexicon: 'casa',
-            translation: 'house',
-          },
-          {
-            ipa: 'es',
-            lemma: 'ser',
-            lexicon: 'es',
-            translation: 'is',
-          },
-          {
-            ipa: 'ˈɡɾande',
-            lemma: 'grande',
-            lexicon: 'grande',
-            translation: 'big',
-          },
-        ],
-        sentence: 'La casa es grande.',
-      },
-      {
-        lexicons: [
-          {
-            ipa: 'el',
-            lemma: 'el',
-            lexicon: 'El',
-            translation: 'The',
-          },
-          {
-            ipa: 'ˈɡato',
-            lemma: 'gato',
-            lexicon: 'gato',
-            translation: 'cat',
-          },
-          {
-            ipa: 'ˈkome',
-            lemma: 'comer',
-            lexicon: 'come',
-            translation: 'eats',
-          },
-        ],
-        sentence: 'El gato come.',
-      },
-      {
-        lexicons: [
-          {
-            ipa: 'ˈeʝa',
-            lemma: 'ella',
-            lexicon: 'Ella',
-            translation: 'She',
-          },
-          {
-            ipa: 'es',
-            lemma: 'ser',
-            lexicon: 'es',
-            translation: 'is',
-          },
-          {
-            ipa: 'mi',
-            lemma: 'mío',
-            lexicon: 'mi',
-            translation: 'my',
-          },
-          {
-            ipa: 'aˈmiɣa',
-            lemma: 'amiga',
-            lexicon: 'amiga',
-            translation: 'friend',
-          },
-        ],
-        sentence: 'Ella es mi amiga.',
-      },
-      {
-        lexicons: [
-          {
-            ipa: 'ˈteŋɡo',
-            lemma: 'tener',
-            lexicon: 'Tengo',
-            translation: 'I have',
-          },
-          {
-            ipa: 'un',
-            lemma: 'un',
-            lexicon: 'un',
-            translation: 'a',
-          },
-          {
-            ipa: 'ˈliβɾo',
-            lemma: 'libro',
-            lexicon: 'libro',
-            translation: 'book',
-          },
-        ],
-        sentence: 'Tengo un libro.',
-      },
-      {
-        lexicons: [
-          {
-            ipa: 'el',
-            lemma: 'el',
-            lexicon: 'El',
-            translation: 'The',
-          },
-          {
-            ipa: 'ˈpero',
-            lemma: 'perro',
-            lexicon: 'perro',
-            translation: 'dog',
-          },
-          {
-            ipa: 'es',
-            lemma: 'ser',
-            lexicon: 'es',
-            translation: 'is',
-          },
-          {
-            ipa: 'boˈnito',
-            lemma: 'bonito',
-            lexicon: 'bonito',
-            translation: 'cute',
-          },
-        ],
-        sentence: 'El perro es bonito.',
-      },
-    ],
-  });
-  // const body = await req.json();
-  // const { knownVocabs, practiceVocabs, settings } = await generateSentenceBody.parseAsync(body);
-  // console.log('Generate Sentences', body);
-
-  // const prompt = buildPrompt({ knownVocabs, practiceVocabs, settings });
-  // const result = await generateObject({
-  //   model: openai('gpt-4o'),
-  //   prompt,
-  //   schema: generateSentenceObjectSchema,
-  // });
-
-  // const newVocabs = result.object.sentences.flatMap((sentence) => sentence.lexicons.flatMap((lexicon) => lexicon.lemma));
-  // const uniqueVocabs = newVocabs.filter((vocab) => !practiceVocabs.includes(vocab));
   // return NextResponse.json({
-  //   knownVocabs,
-  //   practiceVocabs: [...practiceVocabs, ...uniqueVocabs],
-  //   sentences: result.object.sentences,
-  // } satisfies GenerateSentenceApiResponse);
+  //   knownVocabs: [],
+  //   practiceVocabs: [
+  //     'el',
+  //     'casa',
+  //     'ser',
+  //     'grande',
+  //     'el',
+  //     'gato',
+  //     'comer',
+  //     'ella',
+  //     'ser',
+  //     'mío',
+  //     'amiga',
+  //     'tener',
+  //     'un',
+  //     'libro',
+  //     'el',
+  //     'perro',
+  //     'ser',
+  //     'bonito',
+  //   ],
+  //   sentences: [
+  //     {
+  //       lexicons: [
+  //         {
+  //           ipa: 'la',
+  //           lemma: 'el',
+  //           lexicon: 'La',
+  //           translation: 'The',
+  //         },
+  //         {
+  //           ipa: 'ˈkasa',
+  //           lemma: 'casa',
+  //           lexicon: 'casa',
+  //           translation: 'house',
+  //         },
+  //         {
+  //           ipa: 'es',
+  //           lemma: 'ser',
+  //           lexicon: 'es',
+  //           translation: 'is',
+  //         },
+  //         {
+  //           ipa: 'ˈɡɾande',
+  //           lemma: 'grande',
+  //           lexicon: 'grande',
+  //           translation: 'big',
+  //         },
+  //       ],
+  //       sentence: 'La casa es grande.',
+  //     },
+  //     {
+  //       lexicons: [
+  //         {
+  //           ipa: 'el',
+  //           lemma: 'el',
+  //           lexicon: 'El',
+  //           translation: 'The',
+  //         },
+  //         {
+  //           ipa: 'ˈɡato',
+  //           lemma: 'gato',
+  //           lexicon: 'gato',
+  //           translation: 'cat',
+  //         },
+  //         {
+  //           ipa: 'ˈkome',
+  //           lemma: 'comer',
+  //           lexicon: 'come',
+  //           translation: 'eats',
+  //         },
+  //       ],
+  //       sentence: 'El gato come.',
+  //     },
+  //     {
+  //       lexicons: [
+  //         {
+  //           ipa: 'ˈeʝa',
+  //           lemma: 'ella',
+  //           lexicon: 'Ella',
+  //           translation: 'She',
+  //         },
+  //         {
+  //           ipa: 'es',
+  //           lemma: 'ser',
+  //           lexicon: 'es',
+  //           translation: 'is',
+  //         },
+  //         {
+  //           ipa: 'mi',
+  //           lemma: 'mío',
+  //           lexicon: 'mi',
+  //           translation: 'my',
+  //         },
+  //         {
+  //           ipa: 'aˈmiɣa',
+  //           lemma: 'amiga',
+  //           lexicon: 'amiga',
+  //           translation: 'friend',
+  //         },
+  //       ],
+  //       sentence: 'Ella es mi amiga.',
+  //     },
+  //     {
+  //       lexicons: [
+  //         {
+  //           ipa: 'ˈteŋɡo',
+  //           lemma: 'tener',
+  //           lexicon: 'Tengo',
+  //           translation: 'I have',
+  //         },
+  //         {
+  //           ipa: 'un',
+  //           lemma: 'un',
+  //           lexicon: 'un',
+  //           translation: 'a',
+  //         },
+  //         {
+  //           ipa: 'ˈliβɾo',
+  //           lemma: 'libro',
+  //           lexicon: 'libro',
+  //           translation: 'book',
+  //         },
+  //       ],
+  //       sentence: 'Tengo un libro.',
+  //     },
+  //     {
+  //       lexicons: [
+  //         {
+  //           ipa: 'el',
+  //           lemma: 'el',
+  //           lexicon: 'El',
+  //           translation: 'The',
+  //         },
+  //         {
+  //           ipa: 'ˈpero',
+  //           lemma: 'perro',
+  //           lexicon: 'perro',
+  //           translation: 'dog',
+  //         },
+  //         {
+  //           ipa: 'es',
+  //           lemma: 'ser',
+  //           lexicon: 'es',
+  //           translation: 'is',
+  //         },
+  //         {
+  //           ipa: 'boˈnito',
+  //           lemma: 'bonito',
+  //           lexicon: 'bonito',
+  //           translation: 'cute',
+  //         },
+  //       ],
+  //       sentence: 'El perro es bonito.',
+  //     },
+  //   ],
+  // });
+  const body = await req.json();
+  const { knownVocabs, practiceVocabs, settings } = await generateSentenceBody.parseAsync(body);
+  console.log('Generate Sentences', body);
+
+  const prompt = buildPrompt({ knownVocabs, practiceVocabs, settings });
+  const result = await generateObject({
+    model: openai('gpt-4o'),
+    prompt,
+    schema: generateSentenceObjectSchema,
+  });
+
+  const newVocabs = result.object.sentences.flatMap((sentence) => sentence.lexicons.flatMap((lexicon) => lexicon.lemma));
+  const uniqueVocabs = newVocabs.filter((vocab) => !practiceVocabs.includes(vocab));
+  return NextResponse.json({
+    knownVocabs,
+    practiceVocabs: [...practiceVocabs, ...uniqueVocabs],
+    sentences: result.object.sentences,
+  } satisfies GenerateSentenceApiResponse);
 };
