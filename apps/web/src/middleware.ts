9fdcaff05b945b24f1bd4cfe51b6e0f40d895172
Rohid <rohidul209@gmail.com>
2024-09-22 01:04:11 +0600

    #

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -8,16 +8,30 @@ import { routing } from "./i18n/routing";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const pathParts = req.nextUrl.pathname.split("/");
+const intlMiddleware = createMiddleware(routing);
 
-  if (pathParts[2] === "app" && !req.auth) {
+const protectedPages = ["/dashboard/*"];
+const authPages = ["/login"];
+
+const testPagesRegex = (pages: string[], pathname: string) => {
+  const regex = `^(/(${routing.locales.join("|")}))?(${pages
+    .map((p) => p.replace("/*", ".*"))
+    .join("|")})/?$`;
+  return new RegExp(regex, "i").test(pathname);
+};
+
+export default auth((req) => {
+  const isAuth = !!req.auth;
+  const isProtectedPage = testPagesRegex(protectedPages, req.nextUrl.pathname);
+  const isAuthPage = testPagesRegex(authPages, req.nextUrl.pathname);
+
+  if (!isAuth && isProtectedPage) {
     const redirectUrl = new URL("/login", req.nextUrl.origin);
     redirectUrl.searchParams.set("callbackUrl", req.url);
     return NextResponse.redirect(redirectUrl, 307);
   }
 
-  if (pathParts[2] === "login" && req.auth) {
+  if (isAuth && isAuthPage) {
     const redirectUrl = new URL(
       req.nextUrl.searchParams.get("callbackUrl") ?? "/app",
       req.nextUrl.origin,
@@ -25,10 +39,10 @@ export default auth((req) => {
     return NextResponse.redirect(redirectUrl, 307);
   }
 
-  return createMiddleware(routing)(req);
+  return intlMiddleware(req);
 });
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
