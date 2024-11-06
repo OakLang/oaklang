f1af8a9df2930cdb45490204721b2602b05b455a
Rohid <rohidul209@gmail.com>
2024-11-06 11:26:13 +0600

    lint

--- apps/web/src/components/ui/sheet.tsx
+++ apps/web/src/components/ui/sheet.tsx
@@ -4,7 +4,6 @@ import type { VariantProps } from "class-variance-authority";
 import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { cva } from "class-variance-authority";
-import { X } from "lucide-react";
 
 import { cn } from "~/utils";
 
